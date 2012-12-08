6bb219753624d6744332a828c188a987c5d4b3d9
Kevin Sihlanick <kevin.sihlanick@gmail.com>
2012-12-08 12:20:17 -0500

    initial commit of simple bootstrap marketing page example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ErickevinBootstrap::Application.config.secret_token = '743a62e2f1cfb64f43b1bddcddcdf66838356debb2d3e34f9e863a78bd0825696d5f87d6d7a9cf3dfd09abe8b36fbcb685a122768a7f035dcc001ac91ba266b7'
